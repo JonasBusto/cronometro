32cb4f2e5a9a10183d0dfbe160ea6e89fd9c3cb5
Jonas Busto <jonasrex00@gmail.com>
2022-09-04 21:58:56 -0300

    v2

--- script/index.js
+++ script/index.js
@@ -4,60 +4,62 @@ const btnPausar = document.getElementById("btn-pausar");
 const btnDetener = document.getElementById("btn-detener");
 const tablaCuerpo = document.getElementById("tabla-cuerpo");
 let contadorVueltas = 0;
-let contadorMiliSeg = 0;
+let contadorCentesimaSeg = 0;
 let contadorSeg = 0;
 let contadorMin = 0;
 let contadorHora = 0;
-let setIntervalCronometro, setIntervalMiliSeg;
+let setIntervalCronometro;
 
 btnPausar.disabled = true;
 btnDetener.disabled = true;
 
 const cronometrar = () => {
     setIntervalCronometro = setInterval(() => {
-        setIntervalMiliSeg = setTimeout(() => {
-            
-        }, 1);
-
-        contadorSeg++;
-        if (contadorSeg >= 60) {
-            contadorMin++;
-            contadorSeg = 0;
-            if (contadorMin >= 60) {
-                contadorHora++;
-                contadorMin = 0;
+        contadorCentesimaSeg++;
+        if(contadorCentesimaSeg >= 100) {
+            contadorSeg++;
+            contadorCentesimaSeg = 0;
+            if (contadorSeg >= 60) {
+                contadorMin++;
+                contadorSeg = 0;
+                if (contadorMin >= 60) {
+                    contadorHora++;
+                    contadorMin = 0;
+                }
             }
         }
+
         if (contadorHora < 10) {
             if (contadorMin < 10) {
                 if (contadorSeg < 10) {
-                    cronometro.innerHTML = `0${contadorHora} : 0${contadorMin} : 0${contadorSeg}`;
+                    cronometro.innerHTML = `0${contadorHora} : 0${contadorMin} : 0${contadorSeg} : ${contadorCentesimaSeg}`;
                 } else {
-                    cronometro.innerHTML = `0${contadorHora} : 0${contadorMin} : ${contadorSeg}`;
+                    cronometro.innerHTML = `0${contadorHora} : 0${contadorMin} : ${contadorSeg} : ${contadorCentesimaSeg}`;
                 }
             } else {
                 if (contadorSeg < 10) {
-                    cronometro.innerHTML = `0${contadorHora} : ${contadorMin} : 0${contadorSeg}`;
+                    cronometro.innerHTML = `0${contadorHora} : ${contadorMin} : 0${contadorSeg} : ${contadorCentesimaSeg}`;
                 } else {
-                    cronometro.innerHTML = `0${contadorHora} : ${contadorMin} : ${contadorSeg}`;
+                    cronometro.innerHTML = `0${contadorHora} : ${contadorMin} : ${contadorSeg} : ${contadorCentesimaSeg}`;
                 }
             }
         } else {
             if (contadorMin < 10) {
                 if (contadorSeg < 10) {
-                    cronometro.innerHTML = `${contadorHora} : 0${contadorMin} : 0${contadorSeg}`;
+                    cronometro.innerHTML = `${contadorHora} : 0${contadorMin} : 0${contadorSeg} : ${contadorCentesimaSeg}`;
                 } else {
-                    cronometro.innerHTML = `${contadorHora} : 0${contadorMin} : ${contadorSeg}`;
+                    cronometro.innerHTML = `${contadorHora} : 0${contadorMin} : ${contadorSeg} : ${contadorCentesimaSeg}`;
                 }
             } else {
                 if (contadorSeg < 10) {
-                    cronometro.innerHTML = `${contadorHora} : ${contadorMin} : 0${contadorSeg}`;
+                    cronometro.innerHTML = `${contadorHora} : ${contadorMin} : 0${contadorSeg} : ${contadorCentesimaSeg}`;
                 } else {
-                    cronometro.innerHTML = `${contadorHora} : ${contadorMin} : ${contadorSeg}`;
+                    cronometro.innerHTML = `${contadorHora} : ${contadorMin} : ${contadorSeg} : ${contadorCentesimaSeg}s`;
                 }
             }
         }
-    }, 1000);
+        
+    }, 10);
 }
 
 btnIniciar.addEventListener("click", () => {
@@ -74,16 +76,19 @@ btnDetener.addEventListener("click", () => {
     clearInterval(setIntervalCronometro);
     setIntervalCronometro = null;
     tablaCuerpo.innerHTML = "";
-    cronometro.innerHTML = `00 : 00 :00`;
+    cronometro.innerHTML = `00 : 00 : 00 : 00`;
     contadorHora = 0;
     contadorMin = 0;
     contadorSeg = 0;
+    contadorCentesimaSeg = 0;
+    contadorVueltas = 0;
+    btnPausar.innerText = "Pausar";
 })
 
 btnPausar.addEventListener("click", () => {
     if (btnPausar.innerText === "Reanudar") {
         cronometrar();
-        btnPausar.innerText = "Pausar"
+        btnPausar.innerText = "Pausar";
     } else {
         const tr = document.createElement("tr");
         contadorVueltas++;
